63f00e7351f1af0ce796394786a45819c4a4172b
lautimoreyra10 <lauti_15@hotmail.es>
2021-06-01 17:23:13 -0300

    Fase 2 de Mercado Liebre, js con su servidor, html con la estructura y css con sus estilos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    let htmlPath = path.resolve(__dirname, './views/index.html');
+    res.sendFile(htmlPath);
+});
